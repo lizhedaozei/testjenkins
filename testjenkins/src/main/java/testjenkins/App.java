b8b1621678d84bdc0099661938ff78ad215446ad
Lizhe <Lizhe@Lizhe-PC>
2017-11-30 14:28:03 +0800

    version 3

--- testjenkins/src/main/java/testjenkins/App.java
+++ testjenkins/src/main/java/testjenkins/App.java
@@ -4,7 +4,7 @@ public class App {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world");
-		System.out.println("Hello world version2");
+		System.out.println("Hello world version3");
 	}
 
 }
